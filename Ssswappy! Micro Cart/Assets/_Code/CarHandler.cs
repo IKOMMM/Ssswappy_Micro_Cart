71aa0f5a96f450850e0b6c75efc4384f219b8835
IKOMMM <mikolaj.musialik.media@gmail.com>
2021-07-18 12:53:48 +0200

    Added Steering The Car

--- Ssswappy! Micro Cart/Assets/_Code/CarHandler.cs
+++ Ssswappy! Micro Cart/Assets/_Code/CarHandler.cs
@@ -7,11 +7,22 @@ public class CarHandler : MonoBehaviour
     [SerializeField] private float carSpeed = 10f;
     [SerializeField] private float carSpeedIncrease = 0.15f;
 
+    [SerializeField] private int carSteerValue = 250;
+    [SerializeField] private int steerValue = 250;
+
+
         
     void Update()
     {
+        //Speed Increase
         carSpeed += carSpeedIncrease * Time.deltaTime;
-
         transform.Translate(Vector3.forward * carSpeed * Time.deltaTime);
+        //Car Rotation
+        transform.Rotate(0f, steerValue * carSteerValue * Time.deltaTime, 0f);
+    }
+
+    public void Steer(int value)
+    {
+        steerValue = value;
     }
 }
